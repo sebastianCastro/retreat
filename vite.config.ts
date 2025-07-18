2728e18bcf71dd11f0d3621c3fba18061ae58fe3
Sebastian <sebas.c1309@gmail.com>
2025-07-17 21:37:54 -0600

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Retreat/',
+  base: '/retreat/',
   server: {
     host: "::",
     port: 8080,
